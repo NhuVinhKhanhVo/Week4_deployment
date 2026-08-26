044d81d777534887f6a80019a9fb181956fb87b7
NhuVinhKhanhVo <bestplayerever112@gmail.com>
2026-08-26 18:37:27 +1000

    change

--- W3/phonebook/src/App.jsx
+++ W3/phonebook/src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
 
     useEffect(() => {
       axios
-      .get("http://localhost:3001/api/persons")
+      .get("/api/persons")
       .then(response => {
         setPersons(response.data)
       })
